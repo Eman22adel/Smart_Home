f80b7ec6b0c9ea7a792de9a2c5c3bb9db87c899f
eman adel salah <eman.adel24899@gmail.com>
2024-05-11 02:04:57 +0300

    init commit

---
+++ smart home project/code/Master code/Master code/Master code/LCD_config.h
@@ -0,0 +1,14 @@
+/*
+ * LCD_config.h
+ *
+ * Created: 2/23/2022
+ * Author: Eman Adel
+ */ 
+
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+#define eight_bits_mode
+
+#endif /* LCD_CONFIG_H_ */
